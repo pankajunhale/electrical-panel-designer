63fc4aba88e803f7e909695e04f035d0a08b4c8a
pankajunhale <pankajunhale@gmail.com>
2025-07-28 21:35:59 +0530

    remove unused tables

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,10 @@
 import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 import { loginSchema } from "@/schema/auth";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "credentials",
